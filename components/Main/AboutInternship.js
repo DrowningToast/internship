5f22cd51d8149558384f33781bf5e8c1e3037698
DrowningToast <supratouchgussii@gmail.com>
2021-08-07 04:11:08 +0700

    Fix | About Picture Size

---
+++ components/Main/AboutInternship.js
@@ -0,0 +1,11 @@
+const AboutInternship = () => {
+  return (
+    <div className="h-screen w-full grid place-items-center py-20 px-24 relative">
+      <div className="w-full h-3/5 grid gap-x-24 lg:grid-cols-2 lg:grid-rows-1 items-end ">
+        {" "}
+      </div>
+    </div>
+  );
+};
+
+export default AboutInternship;
