a4966a9c00f8da9a31391e0f392aa967b796ae1b
Chanotai Krajeam <boom9387@hotmail.com>
2021-09-13 10:31:38 +0700

    fix: CSS styles flex gap

--- components/Personal/Categories.js
+++ components/Personal/Categories.js
@@ -13,9 +13,9 @@ const Categories = ({
       <h1 className="text-2xl font-medium text-gray-500 text-center md:text-left">
         Categories
       </h1>
-      <div className="flex flex-row flex-wrap justify-center gap-4 md:gap-8 lg:gap-16 mt-8">
+      <div className="flex flex-row flex-wrap justify-center mt-8">
         <button
-          className="relative rounded-3xl cursor-pointer transform hover:translate-x-1 hover:translate-y-1 transition duration-300 focus:ring-2 focus:ring-offset-2 focus:ring-purple-600 h-24 w-24 lg:h-44 lg:w-44 outline-none"
+          className="relative m-4 md:m-8 lg:m-16 rounded-3xl cursor-pointer transform hover:translate-x-1 hover:translate-y-1 transition duration-300 focus:ring-2 focus:ring-offset-2 focus:ring-purple-600 h-24 w-24 lg:h-44 lg:w-44 outline-none"
           style={{ backgroundColor: "#7246D1" }}
           onClick={scrollToArticles}
         >
@@ -27,7 +27,7 @@ const Categories = ({
           </div>
         </button>
         <button
-          className="relative rounded-3xl cursor-pointer transform hover:translate-x-1 hover:translate-y-1 transition duration-300 focus:ring-2 focus:ring-offset-2 focus:ring-purple-600 h-24 w-24 lg:h-44 lg:w-44 outline-none"
+          className="relative m-4 md:m-8 lg:m-16 rounded-3xl cursor-pointer transform hover:translate-x-1 hover:translate-y-1 transition duration-300 focus:ring-2 focus:ring-offset-2 focus:ring-purple-600 h-24 w-24 lg:h-44 lg:w-44 outline-none"
           style={{ backgroundColor: "#7246D1" }}
           onClick={scrollToPictures}
         >
@@ -50,7 +50,7 @@ const Categories = ({
           </div>
         </button>
         <button
-          className="relative rounded-3xl cursor-pointer transform hover:translate-x-1 hover:translate-y-1 transition duration-300 focus:ring-2 focus:ring-offset-2 focus:ring-purple-600 h-24 w-24 lg:h-44 lg:w-44 outline-none"
+          className="relative m-4 md:m-8 lg:m-16 rounded-3xl cursor-pointer transform hover:translate-x-1 hover:translate-y-1 transition duration-300 focus:ring-2 focus:ring-offset-2 focus:ring-purple-600 h-24 w-24 lg:h-44 lg:w-44 outline-none"
           style={{ backgroundColor: "#7246D1" }}
           onClick={scrollToCompany}
         >
@@ -62,7 +62,7 @@ const Categories = ({
           </div>
         </button>
         <button
-          className="relative rounded-3xl cursor-pointer transform hover:translate-x-1 hover:translate-y-1 transition duration-300 focus:ring-2 focus:ring-offset-2 focus:ring-purple-600 h-24 w-24 lg:h-44 lg:w-44 outline-none"
+          className="relative m-4 md:m-8 lg:m-16 rounded-3xl cursor-pointer transform hover:translate-x-1 hover:translate-y-1 transition duration-300 focus:ring-2 focus:ring-offset-2 focus:ring-purple-600 h-24 w-24 lg:h-44 lg:w-44 outline-none"
           style={{ backgroundColor: "#7246D1" }}
           onClick={scrollToConclusion}
         >
@@ -79,7 +79,7 @@ const Categories = ({
           </div>
         </button>
         <button
-          className="relative rounded-3xl cursor-pointer transform hover:translate-x-1 hover:translate-y-1 transition duration-300 focus:ring-2 focus:ring-offset-2 focus:ring-purple-600 h-24 w-24 lg:h-44 lg:w-44 outline-none"
+          className="relative m-4 md:m-8 lg:m-16 rounded-3xl cursor-pointer transform hover:translate-x-1 hover:translate-y-1 transition duration-300 focus:ring-2 focus:ring-offset-2 focus:ring-purple-600 h-24 w-24 lg:h-44 lg:w-44 outline-none"
           style={{ backgroundColor: "#7246D1" }}
           onClick={scrollToContact}
         >
